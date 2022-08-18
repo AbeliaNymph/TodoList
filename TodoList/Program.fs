1520ad2101f845b7e369e2f348bfd63be9f1a9b7
AbeliaNymph <abelianymph@foxmail.com>
2022-08-18 21:38:13 +0800

    新功能：添加了查看待办事项的功能

--- TodoList/Program.fs
+++ TodoList/Program.fs
@@ -1,11 +1,5 @@
 ﻿open System
 
-let help _ = "help"
-
-let unknow_command command = $"未知命令：{command}"
-
-let unimplement_method command = $"未实现的方法{command}"
-
 type Item = 
     {
         id: string
@@ -14,13 +8,98 @@ type Item =
         is_done: bool
     }
 
+    member this.to_csv_string() =
+        $"{this.id},{this.title},{this.content},{this.is_done}\r\n"
+
+module Item =
+    let create id title content is_done = 
+        {
+            id = id
+            title= title
+            content = content
+            is_done = is_done
+        }
+
+    let default_add title content = create (Nanoid.Nanoid.Generate()) title content false
+
+    let from_csv_str (str: string) = 
+        let csv = str.Split ","
+        
+        create csv.[0] csv.[1] csv.[2] (Boolean.Parse csv.[3])
+
+    let display item =
+        if item.is_done then
+            $"[x] {item.id} {item.title} {item.content}"
+        else
+            $"[ ] {item.id} {item.title} {item.content}"
+
+let load_item_list_from_file _ = 
+    let input = System.IO.File.ReadAllText "todo_list.txt"
+
+    let lines = input.Trim().Split("\r\n")
+    
+
+    lines
+    |> Seq.map (fun line -> line.Trim())
+    |> Seq.map Item.from_csv_str
+    |> Seq.toList
+    |> Ok
+
+    
+
 let to_list array = 
     if Array.isEmpty array then
         List.empty
     else
         List.ofArray array
 
-let add list = Ok "add"
+let validate_add param = 
+    match List.length param with
+    | 2 -> 
+        Item.default_add param.[0] param.[1] |> Ok
+    | _ -> Tips.unexpect_param_amount 2 (List.length param) |> Error
+
+let add item_result = 
+    let csv_lines = System.IO.File.ReadAllLines "todo_list.txt"
+
+    let save path string_array = 
+        System.IO.File.WriteAllLines(path, string_array)
+        Ok "保存成功"
+
+    match item_result with
+    | Error e -> Error e
+    | Ok (item:Item) -> 
+        let old_csv_list = csv_lines |> List.ofArray
+        let new_csv_list = item.to_csv_string() :: old_csv_list
+        
+        new_csv_list
+        |> Array.ofList
+        |> save "todo_list.txt"
+        
+        
+let validate_list param = 
+    match List.length param with
+    | 0 ->
+        Ok param
+    | _ -> Tips.unexpect_param_amount 0 (List.length param) |> Error
+    
+
+let list _ = 
+    let str_seq = 
+        load_item_list_from_file()
+        |> Result.defaultValue List.empty
+        |> Seq.map Item.display
+        |> Seq.reduce (fun a b -> a + "\r\n" + b)
+
+    match String.length str_seq with
+    | 0 -> Error "暂无待办事项"
+    | _ -> Ok str_seq
+    
+    
+let validate_done list = 
+    match List.length list with
+    | 1 ->
+    | _ -> Tips.unexpect_param_amount 1 (List.length list) |> Error
 
 
 [<EntryPoint>]
@@ -29,16 +108,18 @@ let main argv =
     
     let result = 
         match input with
-        | [] -> help() |> Error
+        | [] -> Tips.help() |> Error
         | head::tail -> 
             match head with
-            | "add" -> add tail
-            | "list" -> failwith (unimplement_method head)
-            | "done" -> failwith (unimplement_method head)
-            | "rm" -> failwith (unimplement_method head)
-            | _ ->  unknow_command head |> Error
+            | "add" -> validate_add tail |> add
+            | "list" -> 
+                validate_list tail |> ignore 
+                list ()
+            | "done" -> validate_done tail |> done_todo
+            | "rm" -> failwith (Tips.unimplement_method head)
+            | _ ->  Tips.unknow_command head |> Error
 
     match result with
-    | Ok t -> printfn "%A" t
-    | Error e -> eprintfn "%A" e
+    | Ok t -> printfn "%s" t
+    | Error e -> eprintfn "%s" e
     0
---
+++ TodoList/Tips.fs
@@ -0,0 +1,11 @@
+﻿module Tips
+let help _ = "help"
+
+let unknow_command command = $"未知命令：{command}"
+
+let unimplement_method command = $"未实现的方法{command}"
+
+let unexpect_param_amount expect_amount actually_amount = 
+    $"错误：期待{expect_amount}个参数，实际{actually_amount}个参数。"
+
+
