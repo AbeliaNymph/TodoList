ce01a69424a796d5a045accc4f972967483f5c85
AbeliaNymph <abelianymph@foxmail.com>
2022-09-10 21:54:01 +0800

    添加错误信息的红色字体输出

--- TodoList/Tips.fs
+++ TodoList/Tips.fs
@@ -8,4 +8,7 @@ let unimplement_method command = $"未实现的方法{command}"
 let unexpect_param_amount expect_amount actually_amount = 
     $"错误：期待{expect_amount}个参数，实际{actually_amount}个参数。"
 
+let data_file_not_found _ =
+    $"错误：未找到todo_list.txt文件。"
+
 
